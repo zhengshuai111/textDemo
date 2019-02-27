e4d5ea0f182ba3741d729791df9529c889f74a81
JiaJingRu <1240854882@qq.com>
2019-02-27 18:41:15 +0800

    wqeqweq

--- app/src/main/java/com/example/administrator/textdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/textdemo/MainActivity.java
@@ -10,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //好的很
+        //好的很
+        //好的很
+        //好的很
+        //好的很
+        //好的很
 
 
     }
